4e5464b0c64c926c2c4602ec9b7ccd32cdb354a4
Mohamed Ibrahim <mohamed.ibr1994@gmail.com>
2023-03-07 09:01:03 +0200

    remove JsonResource macros.

--- src/AdvancedJsonResourceServiceProvider.php
+++ src/AdvancedJsonResourceServiceProvider.php
@@ -2,31 +2,9 @@
 
 namespace AdvancedJsonResource;
 
-use Illuminate\Http\Resources\Json\JsonResource;
 use Illuminate\Support\ServiceProvider;
 
 class AdvancedJsonResourceServiceProvider extends ServiceProvider
 {
-    /**
-     * List of response methods to register.
-     *
-     * @var array<int, string>
-     */
-    private $responseMethods = [
-        'index', 'show', 'form',
-    ];
-
-    /**
-     * Bootstrap any application services.
-     *
-     * @return void
-     */
-    public function boot(): void
-    {
-        foreach ($this->responseMethods as $method) {
-            JsonResource::macro($method, static function (...$parameters) use($method) {
-                return static::init($method, ...$parameters);
-            });
-        }
-    }
+    //
 }
